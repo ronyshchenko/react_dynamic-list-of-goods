6e96f3656b37f47aaff04f56eed7d852ea8e05c8
ronyshchenko <roma.onyshchenko@gmail.com>
2020-09-30 22:50:33 +0300

    Updates

---
+++ precache-manifest.6be26125c7313916810990f1aa439a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1365f1b584f3d7554422ac331e74f428",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb7fa6269a564ee6dcd0",
+    "url": "./static/css/main.df4349bc.chunk.css"
+  },
+  {
+    "revision": "c48cb556600ca44268f7",
+    "url": "./static/js/2.bb9f0070.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bb9f0070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb7fa6269a564ee6dcd0",
+    "url": "./static/js/main.e5ec99b8.chunk.js"
+  },
+  {
+    "revision": "426178a3e4e034831d22",
+    "url": "./static/js/runtime-main.0c9369d8.js"
+  }
+]);
